22c11e2cb3e910bdaed981495af690fef571f601
H95u <hieu.dh3t1@gmail.com>
2023-07-08 17:18:59 +0700

    update nghe theo list

--- js/listen-playlist.js
+++ js/listen-playlist.js
@@ -2,12 +2,16 @@ function changePage(id) {
     window.location.href = "../login/listen-playlist.html?id=" + id;
 }
 
-function listenPlayList() {
+let albumId;
+let songIndex;
+
+function listenPlayList(index) {
+    songIndex = index;
     const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
+    albumId = urlParams.get('id');
     let content = ""
     $.ajax({
-        url: 'http://localhost:8080/api/song-playlists/' + id,
+        url: 'http://localhost:8080/api/song-playlists/' + albumId,
         type: 'GET',
         success: function (response) {
             content += `<div class="music">
@@ -16,10 +20,10 @@ function listenPlayList() {
     </div>
     <h3 class="music-name">${response.name}</h3>
     <label for="range"></label><input type="range" name="range" id="range" class="range"/>
-    <audio src="${response.songs[0].mp3}" id="song" controls></audio>
+    <audio src="${response.songs[songIndex].mp3}" id="song" controls></audio>
  ${getListMusic(response.songs)}
 </div>`
-            let lyric = `<p>${response.songs[0].lyric}</p>`
+            let lyric = `<p>${response.songs[songIndex].lyric}</p>`
             document.getElementById("play-music").innerHTML = content;
             document.getElementById("lyric").innerHTML = lyric;
             document.getElementById("song").play();
@@ -28,20 +32,20 @@ function listenPlayList() {
 }
 
 function getListMusic(listMusic) {
-    let content = `<hr><table  style="width: 300px">`
+    let content = `<hr><table class="table" >`
     for (let i = 0; i < listMusic.length; i++) {
-        content += getSongPlaylist(listMusic[i])
+        content += getSongPlaylist(listMusic[i],i)
     }
     content += `</table>`
     return content;
 }
 
-function getSongPlaylist(song) {
+function getSongPlaylist(song,index) {
     return `<tr>
 <td><img src="${song.img}"></td>
 <th>${song.name}</th>
 <td> - ${song.singer.name}</td>
-<td ><a href="#"> <span class="icon"><i class="bi bi-play-circle"></i></span>
+<td ><a href="#" onclick="listenPlayList(${index})"> <span class="icon"><i class="bi bi-play-circle"></i></span>
 </a></td>
 </tr>`
 }
